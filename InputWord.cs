10bd41ed5b41856c6a83b226bad1150c2902b048
nopemo <oishi.coffee.icetea@gmail.com>
2023-04-09 18:52:35 +0900

    new file:   InputWord.cs 	renamed:    InputWords.cs.meta -> InputWord.cs.meta 	deleted:    InputWords.cs 	new file:   InputWordsField.cs 	new file:   InputWordsField.cs.meta

---
+++ InputWord.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using TMPro;
+
+public class InputWord : MonoBehaviour, IPointerClickHandler
+{
+  public string wordText = "アアア";
+  public int fontSize = 36;
+  GameObject InputWordsField;
+  void Awake()
+  {
+    //change the chlid TMPro text
+    InputWordsField = GameObject.Find("InputWordsField");
+    gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = wordText;
+    gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().fontSize = fontSize;
+    if (wordText.Length > 3)
+    {
+      gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>().fontSize = 32;
+    }
+  }
+
+  public void OnPointerClick(PointerEventData eventData)
+  {
+    Debug.Log("Clicked on InputWord");
+    InputWordsField.GetComponent<InputWordsField>().SetFromWordButton(wordText);
+  }
+}
